f1513269d72893f056aa7a7f6f0e602d57684087
Mert Efe Sevim <mertefesevim@gmail.com>
2017-06-16 01:05:23 +0300

    matching table changed, file reading needs fixing

--- translate.py
+++ translate.py
@@ -1,53 +1,81 @@
-matching = {'A': '.-',  'B': '-...', 'C': '-.-.',
-            'D': '-..', 'E': '.',    'F': '..-.',
-            'G': '--.', 'H': '....', 'I': '..',
-            'J': '.---','K': '-.-',  'L': '.-..',
-            'M': '--',  'N': '-.',   'O': '---',
-            'P': '.--.','Q': '--.-', 'R': '.-.',
-            'S': '...', 'T': '-',    'U': '..-',
-            'V': '...-','W': '.--',  'X': '-..-',
-            'Y': '-.--','Z': '--..', ' ': '/',
-            
-            '0': '-----', '1': '.----', '2': '..---',
-            '3': '...--', '4': '....-', '5': '.....',
-            '6': '-....', '7': '--...', '8': '---..',
-            '9': '----.',
+matching = {
+        'A': '.-',              'a': '.-',
+        'B': '-...',            'b': '-...',
+        'C': '-.-.',            'c': '-.-.',
+        'D': '-..',             'd': '-..',
+        'E': '.',               'e': '.',
+        'F': '..-.',            'f': '..-.',
+        'G': '--.',             'g': '--.',
+        'H': '....',            'h': '....',
+        'I': '..',              'i': '..',
+        'J': '.---',            'j': '.---',
+        'K': '-.-',             'k': '-.-',
+        'L': '.-..',            'l': '.-..',
+        'M': '--',              'm': '--',
+        'N': '-.',              'n': '-.',
+        'O': '---',             'o': '---',
+        'P': '.--.',            'p': '.--.',
+        'Q': '--.-',            'q': '--.-',
+        'R': '.-.',             'r': '.-.',
+        'S': '...',             's': '...',
+        'T': '-',               't': '-',
+        'U': '..-',             'u': '..-',
+        'V': '...-',            'v': '...-',
+        'W': '.--',             'w': '.--',
+        'X': '-..-',            'x': '-..-',
+        'Y': '-.--',            'y': '-.--',
+        'Z': '--..',            'z': '--..',
+        '0': '-----',           ',': '--..--',
+        '1': '.----',           '.': '.-.-.-',
+        '2': '..---',           '?': '..--..',
+        '3': '...--',           ';': '-.-.-.',
+        '4': '....-',           ':': '---...',
+        '5': '.....',           "'": '.----.',
+        '6': '-....',           '-': '-....-',
+        '7': '--...',           '/': '-..-.',
+        '8': '---..',           '(': '-.--.-',
+        '9': '----.',           ')': '-.--.-',
+        ' ': ' ',               '_': '..--.-',
+}
 
-            "." : ".-.-.-", "," : "--..--", ":" : "---...",
-            "?" : "..--..", "'" : ".----.", "-" : "-....-",
-            "/" : "-..-.",  "@" : ".--.-.",
-            "=" : "-...-"
-            }
 
-def translateToMorse():
 
-    entry = input('Entry: ')
+def translateToMorse(entry,fileName=None):
 
-    result = []
-
-    for character in entry:
-        if character == ' ':
-            raise IOError("entry is empty")
-        else:
-            result.append(matching[character.upper()])
-            #not working
-    print(result)
-    return result
-
-def translateFromMorse():
-
-    entry = input('Entry: ')
+    if fileName is None:
+        pass
+    else:
+        entry=fileName[]
 
     result = ""
 
     for character in entry:
         if character == ' ':
+            raise IOError("Entry is empty")
+        else:
+            result += matching[character.upper()]
+    print(result)
+    return result
+
+
+
+def translateFromMorse(entry): #not working
+
+    result = ""
+
+    for character in entry.upper():
+        if character == ' ':
             raise IOError("entry is empty")
         else:
-            result += matching.values([character.upper()])
+            result += matching.get(entry)
 
     print(result)
 
+
 if __name__ == '__main__':
-    translateToMorse()
-    #translateFromMorse()
+    translateToMorse("m",fileName="sample.txt")
+    #translateFromMorse("...---...") #not working
+
+
+    #entry="S"
+    #print(matching.get(entry))
