dba40d94f79e842a21875ebe37f5f4e511e9a7f7
ycy21 <aslanycan@gmail.com>
2017-06-18 11:53:34 +0300

    Setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyMorse',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/MertEfeSevim/pyMorse',
+    license='Free',
+    author='Mert Efe Sevim',
+    author_email='mertefesevim@gmail.com',
+    description='PyMorse is used for translating between latin alphabet and morse alphabet.'
+)
