21c9dcddbb2e9828947c2741fa5075cca226fb4e
Mert Efe Sevim <mertefesevim@gmail.com>
2017-09-05 12:12:15 +0300

    rearrangments

--- morseTranslator.py
+++ morseTranslator.py
@@ -1,5 +1,6 @@
 class pyMorse(object):
-    def morseTranslator(entry=None, fromFile=None, fromMorse=False, writeToFile=None, playSound=False):
+    def morseTranslator(entry=None, fromFile=None, fromMorse=False,
+    writeToFile=None, playSound=False):
 
         """
         Takes one of the parameters(entry or fileName)
@@ -53,43 +54,44 @@ class pyMorse(object):
             '9': '----.',           ')': '-.--.-',
             ' ': ' ',               '_': '..--.-',
             '':''
-    }
+                    }
 
         # Checks if parameters have entered properly
         assert not (entry != None and fromFile != None), "Entry and fileName can not be used together"
         assert not (fromMorse == True and playSound == True), "Only Morse code can be voiced"
 
-        #If fileName parameter is entered, it is going to read as an entry
+        # If fileName parameter is entered, it is going to read as an entry
         if fromFile is not None: entry = open(fromFile, 'r').read()
 
-        #If translation will be going to occur from Morse code to alphabet
+        # If translation will be going to occur from Morse code to alphabet
         if fromMorse is True:
 
             entry = entry.split(" ")
             reversedMatching = {v: k for k, v in matching.items()}
             matching = reversedMatching
 
-        #changes the characters and adds to result
+        # changes the characters and adds to result
         result = ""
 
         for character in entry:
 
-            if character!='' and fromMorse is True:
+            if character != '' and fromMorse is True:
                 result += matching[character]
             else:
                 result += matching[character] + " "
 
-        #writes result to specified file if entered
+        # writes result to specified file if entered
         if writeToFile is not None: newFile = open(writeToFile, 'w').write(result)
 
-        #plays result as sound if fromMorse is False
+        # plays result as sound if fromMorse is False
         if playSound is True:
 
-            try :
-                import pygame, time
+            try:
+                import pygame
+                import time
 
             except ImportError:
-                raise ImportError("You need to import PyGame to use this feature")
+                raise ImportError("Need to import PyGame to use this feature")
 
             else:
                 pygame.mixer.init()
@@ -110,16 +112,15 @@ class pyMorse(object):
                     if character is " ":
                         time.sleep(0.2)
 
-        #print(result)
         return result
 
     if __name__ == '__main__':
         import sys
         get_input = input
 
-        #If python2 is used, func still can work
+        # If python2 is used, func still can work
         if sys.version_info[:2] <= (2, 7):
             get_input = raw_input
 
 
-#pyMorse.morseTranslator(entry="sos", playSound=True)
+print(pyMorse.morseTranslator(entry="sos"))
